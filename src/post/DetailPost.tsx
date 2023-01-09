b12fbad877192fe4b8cf67d02c00ad04b7ea8d44
JaeKwan Ahn <52008@naver.com>
2023-01-10 03:38:27 +0900

    feat: ✨내가 작성한 포스트 수정하기 구현

--- src/post/DetailPost.tsx
+++ src/post/DetailPost.tsx
@@ -2,12 +2,44 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 
 const END_POINT = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
-const postId = '63bc3d1023d1e65ff38c5e77';
+const postId = '63bc5a9f23d1e65ff38c6b9b';
 
 // 추후에 postId를 DetailPost의 props로 받아와서 보여주는 방식으로 구성하면될듯.
 const DetailPost = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+
+  const handleTitleOnChnage = (e: string) => {
+    setTitle(e);
+  };
+  const handleContentOnChnage = (e: string) => {
+    setContent(e);
+  };
+
+  const token = localStorage.getItem('TOKEN_KEY');
+
+  const UpdatePost = () => {
+    const testData = {
+      title: title,
+      content: content,
+    };
+    const temp = JSON.stringify(testData);
+
+    const post = {
+      postId: postId,
+      title: temp,
+      image: null,
+      //imageToDeletePublicId: Optional<string>,
+      channelId: String,
+    };
+
+    axios.put(`${END_POINT}/posts/update`, post, {
+      headers: {
+        Authorization: `bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+  };
 
   const fetchPost = async () => {
     try {
@@ -28,14 +60,20 @@ const DetailPost = () => {
   useEffect(() => {
     fetchPost();
   }, []);
-  console.log(title, content);
 
   return (
     <div>
       <h1>Detail Post Page</h1>
-      {title}
+      <input value={title} onChange={(e) => handleTitleOnChnage(e.target.value)} />
       <br />
-      {content}
+      <textarea
+        onChange={(e) => handleContentOnChnage(e.target.value)}
+        rows={10}
+        cols={100}
+        value={content}
+        placeholder="내용"
+      />
+      <button onClick={UpdatePost}>내용 수정</button>
     </div>
   );
 };
