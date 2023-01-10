5684296c4163a1b864282e5f4c8322bdfbc3132b
JaeKwan Ahn <52008@naver.com>
2023-01-10 18:40:33 +0900

    feat: ✨API 통신 Hook 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Post from './post';
-import DetailPost from './post/DetailPost';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DetailPost />} />
+        <Route path="/" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/hooks/api.ts
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+const END_POINT = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
+
+interface Iprops {
+  url: string;
+  body: object;
+  headers: object;
+  method: string;
+}
+
+export const useCustomApi = ({ url, body, headers, method }: Iprops) => {
+  // const token = localStorage.getItem('TOKEN_KEY');
+  // if (!token) return console.warn('Need Token');
+
+  switch (method) {
+    case 'GET':
+      try {
+        async () =>
+          await axios
+            .get(`${END_POINT}${url}`, {
+              data: {
+                ...body,
+                ...headers,
+              },
+            })
+            .then((res) => res.data)
+            .catch((e) => console.log(e));
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    case 'POST':
+      try {
+        axios
+          .post(`${END_POINT}${url}`, body, headers)
+          .then((res) => console.log(res))
+          .catch((e) => console.log(e));
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    case 'DELETE':
+      try {
+        axios.delete(`${END_POINT}${url}`, {
+          data: {
+            ...body,
+            ...headers,
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    default:
+      console.log('method is not valid');
+  }
+};
+export default useCustomApi;
