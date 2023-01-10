fefa5f358c37f0e7763c4b01de78f5fa83a5816f
JaeKwan Ahn <52008@naver.com>
2023-01-10 22:35:37 +0900

    feat: ✨API 통신 Hooks 구현

---
+++ src/hooks/api.tsx
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const END_POINT = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
+
+interface Iprops {
+  url: string;
+  method: string;
+  body?: object;
+  headers?: object;
+}
+
+export const useCustomApi = async ({ url, body, headers, method }: Iprops) => {
+  // const token = localStorage.getItem('TOKEN_KEY');
+  // if (!token) return console.warn('Need Token');
+
+  switch (method) {
+    case 'GET':
+      try {
+        return await axios
+          .get(`${END_POINT}${url}`, {
+            data: {
+              ...body,
+              ...headers,
+            },
+          })
+          .then((res) => res.data)
+          .catch((e) => console.log(e));
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    case 'POST':
+      try {
+        axios
+          .post(`${END_POINT}${url}`, body, headers)
+          .then((res) => res.data)
+          .catch((e) => console.log(e));
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    case 'DELETE':
+      try {
+        axios.delete(`${END_POINT}${url}`, {
+          data: {
+            ...body,
+            ...headers,
+          },
+        });
+      } catch (e) {
+        console.log(e);
+      }
+      break;
+    default:
+      console.log('method is not valid');
+  }
+};
+export default useCustomApi;
