d101df221ac6d02ffb70a7a492d74290ce5d6fef
유지영 <yj0zero@gmail.com>
2023-01-10 18:06:17 +0900

    feat: ✨ 유저페이지 라우터 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,9 +7,9 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Post />} />
-        <Route path="/user/:id" element={<User />} />
-        <Route path="/userEdit/:id" element={<UserEdit />} />
+        <Route path='/' element={<Post />} />
+        <Route path='/user/:id' element={<User />} />
+        <Route path='/userEdit/:id' element={<UserEdit />} />
       </Routes>
     </BrowserRouter>
   );
