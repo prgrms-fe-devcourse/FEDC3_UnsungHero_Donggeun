bbc76a071cf1690d459cc2dac63b335bcf523a8a
Park-min-hyoung <qkralswgud1324@naver.com>
2023-01-11 16:23:55 +0900

    feat: :sparkles: 알람 목록 스위치 및 알람 확인

--- src/alarm/AlarmList.tsx
+++ src/alarm/AlarmList.tsx
@@ -19,8 +19,29 @@ const TOKEN_KEY = 'token';
 const AlarmList = () => {
   const [alarmList, setAlarmList] = useState<IAlarm[]>();
   const [alramStatus, setAlramStatus] = useState<boolean | undefined>(true);
+  const [showedAlarm, setShowedAlarm] = useState(false);
 
-  const viewAlarmList = async () => {
+  // by 민형, 이미 모든 알람을 열어본 경우에는 해당 로직이 수행되지 않도록(리팩토링)_230111
+  const confirmAlarmList = async () => {
+    const item = localStorage.getItem(TOKEN_KEY);
+    const storedValue = item ? JSON.parse(item) : '';
+    await axios
+      .put(
+        'http://kdt.frontend.3rd.programmers.co.kr:5006/notifications/seen',
+        {},
+        {
+          headers: { Authorization: `bearer ${storedValue}` },
+        }
+      )
+      .then((res) => {
+        fetchAlarmData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const fetchAlarmData = async () => {
     const item = localStorage.getItem(TOKEN_KEY);
     const storedValue = item ? JSON.parse(item) : '';
 
@@ -36,6 +57,15 @@ const AlarmList = () => {
       });
   };
 
+  const viewAlarmList = async () => {
+    if (showedAlarm) {
+      setShowedAlarm(false);
+      return;
+    }
+
+    setShowedAlarm(true);
+  };
+
   const checkAlarmStatus = () => {
     const seenData = alarmList?.map(({ seen }) => seen);
     const status = seenData?.includes(false);
@@ -43,7 +73,7 @@ const AlarmList = () => {
   };
 
   useEffect(() => {
-    viewAlarmList();
+    fetchAlarmData();
   }, []);
 
   useEffect(() => {
@@ -51,19 +81,31 @@ const AlarmList = () => {
   }, [alarmList]);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center' }}>
-      {alarmList?.map(({ _id, seen, comment }) => (
-        <AlarmListItem _id={_id} seen={seen} comment={comment} />
-      ))}
-      {alramStatus && (
+    <>
+      <button onClick={viewAlarmList} style={{ width: '100vw' }}>
+        알림 목록 리스트 Render
+      </button>
+      {showedAlarm && (
         <>
-          <div>현재 알람 상태</div>
-          <div
-            style={{ width: '100px', height: '100px', borderRadius: '50%', backgroundColor: 'green' }}
-          ></div>
+          <button onClick={confirmAlarmList} style={{ width: '100vw' }}>
+            모든 알림 확인
+          </button>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {alarmList?.map(({ _id, seen, comment }) => (
+              <AlarmListItem _id={_id} seen={seen} comment={comment} />
+            ))}
+            {alramStatus && (
+              <>
+                <div>현재 알람 상태</div>
+                <div
+                  style={{ width: '100px', height: '100px', borderRadius: '50%', backgroundColor: 'green' }}
+                ></div>
+              </>
+            )}
+          </div>
         </>
       )}
-    </div>
+    </>
   );
 };
 
