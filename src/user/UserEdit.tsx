c86d663671b3f6eb4df21162f5ecb7b7e7184857
유지영 <yj0zero@gmail.com>
2023-01-11 21:23:44 +0900

    feat: ✨ react-hook-form 으로 기본 수정 기능 구현

--- src/user/UserEdit.tsx
+++ src/user/UserEdit.tsx
@@ -1,112 +1,81 @@
-import React, { useEffect } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
+const currentName = 'yellow'; //서버에서 받아오는 이름
 const API_URL = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
 const TOKEN = localStorage.getItem('token');
-const header = {
+const headers = {
   Authorization: `bearer ${TOKEN}`,
 };
 
-interface ICheckPassword {
-  validated: boolean;
-  message: string;
+interface IFormValue {
+  fullName: string;
+  password: string;
 }
 
+const id = '63bbb8d08c65a93bebe29f78';
 const UserEdit = () => {
   const navigate = useNavigate();
-  const [userName, setUserName] = useState({
-    currentName: '',
-    newName: '',
+
+  // const { id } = useParams();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormValue>({
+    defaultValues: {
+      fullName: currentName,
+    },
   });
-  const [password, setPassword] = useState('');
-  const { id } = useParams();
 
-  useEffect(() => {
-    axios.get(`${API_URL}/users/${id}`).then(({ data }) =>
-      setUserName({
-        currentName: data.fullName,
-        newName: data.fullName,
-      })
-    );
-  }, []); //다른 컴포넌트와 중복되는 api호출 부분.
-
-  const handleChangeUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName({
-      ...userName,
-      newName: e.target.value,
-    });
-  };
-
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(() => e.target.value);
-  };
-
-  const handleChangeUserInfo = (e: React.MouseEvent) => {
-    e.preventDefault();
-    const { validated, message } = CheckUserPassword(password);
-
-    if (!validated) return alert(message);
-
-    if (userName.currentName !== userName.newName) {
-      //유저이름이 변경되었을 때만 호출
-      getChangeUserName();
-    }
-
-    if (password.length > 0) {
-      //비밀번호가 입력되었을 때만 호출
-      getChangePassword();
-    }
+  const handleChangeUserInfo: SubmitHandler<IFormValue> = ({ fullName, password }) => {
+    if (currentName !== fullName) getChangeUserName(fullName);
+    if (password.length > 0) getChangePassword(password);
 
     navigate(`/user/${id}`);
   };
 
-  const CheckUserPassword = (password: string): ICheckPassword => {
-    if (password.length > 0 && password.length < 7) {
-      return {
-        validated: false,
-        message: '비밀번호는 7글자 이상 입력해주세요!',
-      };
-    }
-
-    return {
-      validated: true,
-      message: '',
-    };
+  const getChangePassword = async (password: string) => {
+    return await axios.put(`${API_URL}/settings/update-password`, { password }, { headers });
   };
 
-  const getChangePassword = async () => {
-    return await axios.put(
-      `${API_URL}/settings/update-password`,
-      {
-        password: password,
-      },
-      {
-        headers: header,
-      }
-    );
-  };
-  const getChangeUserName = async () => {
+  const getChangeUserName = async (fullName: string) => {
     return await axios.put(
       `${API_URL}/settings/update-user`,
       {
-        fullName: userName.newName,
+        fullName,
         username: '',
       },
-      {
-        headers: header,
-      }
+      { headers }
     );
   };
   return (
     <>
       <div>커버이미지</div>
       <div>프로필 이미지</div>
-      <form>
-        <input type='text' value={userName.newName} onChange={handleChangeUserName} />
-        <input type='password' value={password} onChange={handleChangePassword} />
-        <button onClick={handleChangeUserInfo}>저장</button>
+      <form onSubmit={handleSubmit(handleChangeUserInfo)}>
+        <input
+          type='text'
+          {...register('fullName', {
+            minLength: {
+              value: 2,
+              message: '2자리 이상의 이름을 입력해주세요',
+            },
+          })}
+        />
+
+        <input
+          type='password'
+          {...register('password', {
+            minLength: {
+              value: 7,
+              message: '7자리 이상의 비밀번호를 입력해주세요.',
+            },
+          })}
+        />
+        <span>{errors?.password?.message}</span>
+        <button type='submit'>저장</button>
       </form>
     </>
   );
