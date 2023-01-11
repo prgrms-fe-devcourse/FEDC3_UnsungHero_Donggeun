988b2870f9d36c4b906b5847004a429a4b05f47b
DaeHyun_Woo <113018070+WooDaeHyun@users.noreply.github.com>
2023-01-11 14:05:12 +0900

    Feature/search: 상세 검색 기능 및 포스트 목록 등 구현 (#17)
    * feat: ✨상세 검색창 기능 구현
    
    * Fix: 🚑input과 select의 값 전달 오류 해결
    
    * feat: ✨특정 채널의 포스트 목록 불러와 리스트로 보여주기
    
    * chore: 📝렌더링 테스트 코드 삭제
    
    * feat: ✨검색 필터 기능 추가(제목, 제목+내용, 작성자)
    
    * chore: 📝이벤트 핸들러명 통일 및 변수명 변경
    
    * chore: 📝interface명 통일

--- src/post/index.tsx
+++ src/post/index.tsx
@@ -1,5 +1,9 @@
 function Post() {
-  return <h1>언성히어로 최고🤗</h1>;
+  return (
+    <>
+      <h1>언성히어로 최고🤗</h1>
+    </>
+  );
 }
 
 export default Post;
---
+++ src/search/PostList.tsx
@@ -0,0 +1,63 @@
+import { FunctionComponent } from 'react';
+
+interface Ilikes {
+  _id: string;
+}
+interface Iauthor {
+  fullName: string;
+  username: string;
+}
+
+interface IpostsInfo {
+  title: string;
+  _id: string;
+  author: Iauthor;
+  likes: Ilikes[];
+}
+interface IpostListProps {
+  postsInfo: IpostsInfo[];
+  selectedSearchOption: string;
+  inputSearchValue: string;
+}
+
+const PostList: FunctionComponent<IpostListProps> = ({
+  postsInfo,
+  selectedSearchOption,
+  inputSearchValue,
+}) => {
+  return (
+    <ul>
+      {postsInfo
+        .filter((postInfo) => {
+          const { title } = postInfo;
+          const { fullName } = postInfo.author; //fullName이 아니라 userName이 닉네임인 경우 변경해야함
+
+          if (selectedSearchOption === '제목') {
+            return JSON.parse(title).title.includes(inputSearchValue);
+          } else if (selectedSearchOption === '제목+내용') {
+            return (
+              JSON.parse(title).title.includes(inputSearchValue) ||
+              JSON.parse(title).content.includes(inputSearchValue)
+            );
+          } else if (selectedSearchOption === '작성자') {
+            return fullName.includes(inputSearchValue);
+          }
+        })
+        .map((postInfo) => {
+          const { title, _id, likes } = postInfo;
+          const { fullName } = postInfo.author; //fullName이 아니라 userName이 닉네임인 경우 변경해야함
+          return (
+            <li key={_id}>
+              <div>
+                {JSON.parse(title).title}
+                <div>공감수: {likes.length}</div>
+                <div>작성자: {fullName}</div>
+              </div>
+              <div>{JSON.parse(title).content}</div>
+            </li>
+          );
+        })}
+    </ul>
+  );
+};
+export default PostList;
