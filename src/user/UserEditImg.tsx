f755a71534a02865881a308b5bd2f6d1d551115f
유지영 <yj0zero@gmail.com>
2023-01-12 17:25:38 +0900

    feat: ✨ 이미지 수정 api 호출 연결

--- src/user/UserEditImg.tsx
+++ src/user/UserEditImg.tsx
@@ -1,29 +1,64 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
+import useAxios from '../api/useAxios';
+import { IUser } from '../types/user';
+import axios from 'axios';
 
 const COVER_IMG_URL = 'https://ifh.cc/g/ZSypny.png';
 const PROFIE_IMG_URL = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png';
+const API_URL = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
+const TOKEN = localStorage.getItem('token');
+const headers = {
+  Authorization: `bearer ${TOKEN}`,
+  'Content-Type': 'multipart/form-data',
+};
+interface IProps {
+  id: string;
+}
 
-const UserEditImg = () => {
-  const [profileImage, setProfileImage] = useState(PROFIE_IMG_URL);
-  const [coverImage, setCoverImage] = useState(COVER_IMG_URL);
+const UserEditImg = ({ id }: IProps) => {
+  const { data, fetchData } = useAxios<IUser>({
+    url: `${API_URL}/users/${id}`,
+    method: 'get',
+  });
+
+  const [profileImage, setProfileImage] = useState('');
+  const [coverImage, setCoverImage] = useState('');
   const profileInputRef = useRef<HTMLInputElement>(null);
   const coverInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    setCoverImage(data?.coverImage ?? COVER_IMG_URL);
+    setProfileImage(data?.image ?? PROFIE_IMG_URL);
+  }, [data]);
 
   const handleChangeImg = (e: React.ChangeEvent<HTMLInputElement>, type: string) => {
     if (e.currentTarget.files !== null) {
       const file = e.currentTarget.files[0];
 
       const reader = new FileReader();
-      reader.onload = () => {
+      reader.readAsDataURL(file);
+      reader.onload = async () => {
         if (reader.readyState === 2) {
           type === 'profile'
             ? setProfileImage(reader.result as string)
             : setCoverImage(reader.result as string);
         }
       };
-      reader.readAsDataURL(file);
+
+      const formData = new FormData();
+
+      const cover = type === 'profile' ? 'false' : 'true';
+      formData.append('isCover', cover);
+      formData.append('image', file);
+      getChangeImg(formData);
     }
+  };
+
+  const getChangeImg = async (formData: FormData) => {
+    await axios.post(`${API_URL}/users/upload-photo`, formData, { headers });
+
+    fetchData();
   };
 
   return (
@@ -65,4 +100,5 @@ const CoverImg = styled.img`
 const ProfileImg = styled.img`
   border-radius: 50%;
   width: 80px;
+  height: 80px;
 `;
