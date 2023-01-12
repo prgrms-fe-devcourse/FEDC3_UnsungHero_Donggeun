c85ecf6f693d67ddfc06c15cd8c466f35b579a6a
Park-min-hyoung <qkralswgud1324@naver.com>
2023-01-12 21:47:56 +0900

    refactor: 🔨 token 유무에 따라 다르게 render
    유효한 token 값이 있을 때만 알람 목록 리스트 버튼 render
    모든 알람을 확인한 상태라면 알람을 확인하는 API 요청하지 않기

--- src/notification/NotificationList.tsx
+++ src/notification/NotificationList.tsx
@@ -8,13 +8,19 @@ const TOKEN_KEY = 'token';
 
 const NotificationList = () => {
   const [notificationList, setNotificationlist] = useState<INotification[]>();
-  const [notificationStatus, setNotificationStatus] = useState<boolean | undefined>(true);
+  const [notificationStatus, setNotificationStatus] = useState<boolean | undefined>(false);
   const [showedNotificationListStatus, setShowedNotificationListStatus] = useState(false);
+
+  // by 민형, 이 부분은 나중에 token context로 수정_230112
+  const token = localStorage.getItem(TOKEN_KEY);
 
   const { mutate } = useMutation();
 
   // by 민형, 이미 모든 알람을 열어본 경우에는 해당 로직이 수행되지 않도록(리팩토링)_230111
+  // by 민형, 리팩토링 완료_230112
   const confirmNotificationlist = () => {
+    if (notificationStatus) return;
+
     mutate({
       url: `http://kdt.frontend.3rd.programmers.co.kr:5006/notifications/seen`,
       method: 'put',
@@ -42,26 +48,29 @@ const NotificationList = () => {
 
   const updateNotificationStatus = () => {
     const notificationListData = notificationList?.map(({ seen }) => seen);
-    const notificationStatus = notificationListData?.includes(false);
-    setNotificationStatus(notificationStatus);
+    const notificationState = notificationListData?.includes(false);
+    setNotificationStatus(!notificationState);
   };
 
   const toggleShowedNotificationListStatus = () =>
     setShowedNotificationListStatus((prevStatus) => !prevStatus);
 
   useEffect(() => {
-    fetchNotificationData();
+    token !== null && fetchNotificationData();
   }, []);
 
+  // by 민형, notificationList state가 수정되는 경우(token이 있는 경우)에만 fetch 되므로 따로 token check x_230112
   useEffect(() => {
     updateNotificationStatus();
   }, [notificationList]);
 
   return (
     <>
-      <button onClick={toggleShowedNotificationListStatus} style={{ width: '100vw' }}>
-        알림 목록 리스트 Render
-      </button>
+      {token !== null && (
+        <button onClick={toggleShowedNotificationListStatus} style={{ width: '100vw' }}>
+          알림 목록 리스트 Render
+        </button>
+      )}
       {showedNotificationListStatus && (
         <>
           <button onClick={confirmNotificationlist} style={{ width: '100vw' }}>
@@ -71,7 +80,7 @@ const NotificationList = () => {
             {notificationList?.map(({ _id, seen: isCheck, comment }) => (
               <NotificationlistItem key={_id} _id={_id} seen={isCheck} comment={comment} />
             ))}
-            {notificationStatus && (
+            {!notificationStatus && (
               <>
                 <div>현재 알람 상태</div>
                 <div
