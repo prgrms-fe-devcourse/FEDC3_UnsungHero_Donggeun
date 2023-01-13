ee0971a4389c1b729598a6f7a975cb6d295fcfe7
유지영 <yj0zero@gmail.com>
2023-01-13 17:41:39 +0900

    chore: 📝라우터 유저 정보 수정페이지 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,8 +5,7 @@ import User from './user';
 import UpdatePost from './post/UpdatePost';
 import CreatePost from './post/CreatePost';
 import DetailPost from './post/DetailPost';
-
-// import UserEdit from './user/UserEdit';
+import UserEdit from './user/UserEdit';
 
 function Router() {
   return (
@@ -18,7 +17,7 @@ function Router() {
           <Route path='/post/:postId' element={<DetailPost />} />
           <Route path='/post/channelId/updatePost/:postId' element={<UpdatePost />} />
           <Route path='/post/create/:chnnalId' element={<CreatePost />} />
-          {/* <Route path='/userEdit/:id' element={<UserEdit />} /> */}
+          <Route path='/userEdit/:id' element={<UserEdit />} />
         </Routes>
       </Layout>
     </BrowserRouter>
