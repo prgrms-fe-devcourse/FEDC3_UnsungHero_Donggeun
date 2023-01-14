a97db2bf63eea7b33ca56ee8262e113e27587ee5
Park-min-hyoung <qkralswgud1324@naver.com>
2023-01-14 15:04:40 +0900

    refactor: 🔨 로그인 시에만 토큰 사용 가능

--- src/contexts/TokenProvider.tsx
+++ src/contexts/TokenProvider.tsx
@@ -10,12 +10,23 @@ const TOKEN_KEY = 'token';
 
 const TokenProvider = ({ children }: { children: React.ReactNode }) => {
   const [localToken] = useLocalStorage(TOKEN_KEY, '');
+
+  const [checkTokenCertification, setCheckTokenCertification] = useState<boolean>(false);
   const [token, setToken] = useState(localToken);
 
-  const addToken = (getToken: string) => setToken(getToken);
-  const removeToken = () => setToken(null);
+  const addToken = (getToken: string) => {
+    setToken(getToken);
+    setCheckTokenCertification(true);
+  };
+  const removeToken = () => setToken('');
 
-  return <TokenContext.Provider value={{ token, addToken, removeToken }}>{children}</TokenContext.Provider>;
+  return (
+    <TokenContext.Provider
+      value={{ token: checkTokenCertification ? token : undefined, addToken, removeToken }}
+    >
+      {children}
+    </TokenContext.Provider>
+  );
 };
 
 export default TokenProvider;
