98d68146bf3c2e562d983c9f4a87843e9dce53ba
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-01-15 18:48:48 +0900

    Feature/notifications: 알람 관련 추가 기능 구현 (#63)
    * fix: :bug: 알람 버튼 컴포넌트로 분리
    
    * feat: :sparkles: 메인 페이지에 있을 때 알람 받은 경우
    
    메인 페이지에 머무를 때 알람 받은 경우 알람 페이지로 이동하면 알람 표시가 render 되도록
    알람 페이지에서 모든 알람 목록을 확인하면 알람 표시가 제거 되도록
    notificationStatus context 사용
    
    * feat: :sparkles: 실시간 알람 check 버튼
    
    * feat: :sparkles: 알람 관련 나머지 기능
    
    실시간 알람 버튼 클릭 후 모든 알람 확인 버튼 클릭하면 반영 안되는 문제 해결
    알람 관련 내용 render 및 좋아요 알람 인지 댓글 알람 인지 분리
    알림 목록 클릭 후 특정 알람을 클릭하면 해당 포스트로 이동
    
    * * refactor: 🔨 소스코드 정리

---
+++ src/notification/NotificationStatus.tsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useToken } from '../contexts/TokenProvider';
+import { IToken } from '../types/token';
+import { useNotificationStatus } from '../contexts/NotificationStatusProvider';
+import { INotification, INotificationStatus } from '../types/notification';
+import { useState, useEffect } from 'react';
+
+const NotificationStatus = () => {
+  const [notificationStatusList, setNotificationStatusList] = useState<boolean[]>([]);
+  const tokenContextObj: IToken | null = useToken();
+  const notificationStatusContextObj: INotificationStatus | null = useNotificationStatus();
+
+  const fetchNotificationData = async () => {
+    await axios
+      .get('http://kdt.frontend.3rd.programmers.co.kr:5006/notifications', {
+        headers: { Authorization: `bearer ${tokenContextObj?.token}` },
+      })
+      .then((res) => {
+        const serverData = res.data;
+        const allSeenCheckData = serverData.map((data: INotification) => data.seen);
+        setNotificationStatusList(allSeenCheckData);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const checkNotificationStatus = () => {
+    if (notificationStatusList.includes(false)) notificationStatusContextObj?.setNotification(true);
+  };
+
+  useEffect(() => {
+    fetchNotificationData();
+  }, []);
+
+  useEffect(() => {
+    checkNotificationStatus();
+  }, [notificationStatusList]);
+
+  return (
+    <>
+      {notificationStatusContextObj?.notificationStatus && (
+        <div style={{ width: '30px', height: '30px', borderRadius: '50%', backgroundColor: 'green' }}></div>
+      )}
+    </>
+  );
+};
+
+export default NotificationStatus;
