7c22d0e6ef0022836ecd442c0cbfe08f5a35e6e2
JaeKwan Ahn <52008@naver.com>
2023-01-16 14:44:13 +0900

    refactor: 📝confilct 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import TokenProvider from './contexts/TokenProvider';
 import Router from './Router';
 import GlobalStyles from './styles/globalStyle';
 import theme from './styles/theme';
+import NotificationStatusProvider from './contexts/NotificationStatusProvider';
 
 function App() {
   return (
-    <TokenProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Router />
-      </ThemeProvider>
-    </TokenProvider>
+    <ThemeProvider theme={theme}>
+      <TokenProvider>
+        <NotificationStatusProvider>
+          <GlobalStyles />
+          <Router />
+        </NotificationStatusProvider>
+      </TokenProvider>
+    </ThemeProvider>
   );
 }
 
