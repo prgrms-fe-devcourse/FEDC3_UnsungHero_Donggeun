54a0ea37e1624e50e80ec8ce614b2fa27e3b319d
JiYoung <yj0zero@gmail.com>
2023-01-17 03:23:14 +0900

    Feature/follow user: 팔로우, 언팔 기능 붙이기 (#81)
    * refactor: 🔨 팔로우 관련 기능 훅으로 수정
    
    * chore: 📝 팔로우 관련 페이지 라우터 주소 수정
    
    * chore: 📝 user 팔로우 관련 타입 변경
    
    * refactor: 🔨 팔로우 관련 기능 커스텀 훅 사용하도록 수정
    
    * fix: 🚑 간헐적으로 이미지 엑박뜨는 현상 수정
    
    * chore: 📝 버튼 이름 수정
    
    * chore: 📝 헤더에 로그아웃 버튼 추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { Logout } from '../auth';
 import { useUserId } from '../contexts/TokenProvider';
 import NotificationStatus from '../notification/NotificationStatus';
 import { IUserId } from '../types/useId';
