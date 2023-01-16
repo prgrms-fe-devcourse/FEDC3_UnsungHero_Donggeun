46cffedbb6cd40c941b3aa07e5f0f561550c2841
유지영 <yj0zero@gmail.com>
2023-01-16 21:22:21 +0900

    refactor: 🔨 팔로우 관련 기능 커스텀 훅 사용하도록 수정

--- src/user/UserFollowing.tsx
+++ src/user/UserFollowing.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useAxios from '../api/useAxios';
+import useFollow from '../follow/useFollow';
 import { IUser } from '../types/user';
 import Pagination from './Pagination';
 
@@ -8,9 +9,9 @@ const API_URL = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
 const PROFIE_IMG_URL = 'https://ifh.cc/g/35RDD6.png';
 
 const UserFollowing = () => {
+  const { id } = useParams();
   const navigate = useNavigate();
-  const { state } = useLocation();
-  const following = state.following;
+  const { followButton, userFollow } = useFollow(id as string);
   const [page, setPage] = useState(1);
   const limit = 10;
   const offset = (page - 1) * limit;
@@ -20,7 +21,7 @@ const UserFollowing = () => {
     method: 'get',
   });
 
-  const followingsList = data?.filter((user: IUser) => following.includes(user._id));
+  const followingsList = data?.filter((user: IUser) => userFollow?.following?.includes(user._id));
   const handleClickUser = (id: string) => {
     navigate(`/user/${id}`);
   };
@@ -31,8 +32,9 @@ const UserFollowing = () => {
       {followingsList &&
         followingsList.slice(offset, offset + limit).map((user: IUser) => (
           <div key={user._id} onClick={() => handleClickUser(user._id)}>
-            <img src={user.image && PROFIE_IMG_URL} width='80px' height='80px' />
+            <img src={user.image && PROFIE_IMG_URL} width='80px' height='80px' alt='프로필 이미지' />
             <span>{user.fullName}</span>
+            {followButton(user._id)}
           </div>
         ))}
       {followingsList && followingsList.length < 1 ? (
