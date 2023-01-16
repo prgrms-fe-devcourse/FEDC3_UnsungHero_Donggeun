45d4551f79afbb7ae46f7923bfc249c758df0d6f
JaeKwan Ahn <52008@naver.com>
2023-01-17 02:43:40 +0900

    feat: ✨detailPost에 작성자 정보 추가. (#80)

--- src/post/DetailPost.tsx
+++ src/post/DetailPost.tsx
@@ -9,6 +9,7 @@ import { IPost } from '../types/post';
 import { useToken } from '../contexts/TokenProvider';
 
 const END_POINT = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
+const PROFIE_IMG_URL = 'https://ifh.cc/g/35RDD6.png';
 
 const Container = styled.div`
   max-width: 50%;
@@ -16,11 +17,24 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
+interface Iauthor {
+  fullName: string;
+  image: string;
+  createAt: string;
+  _id: string;
+}
+
 const DetailPost = () => {
   const navigate = useNavigate();
 
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [author, setAuthor] = useState<Iauthor>({
+    fullName: '',
+    image: PROFIE_IMG_URL,
+    createAt: '',
+    _id: '',
+  });
 
   const { postId } = useParams();
 
@@ -37,6 +51,19 @@ const DetailPost = () => {
       const post = JSON.parse(data?.title as string);
       setTitle(post.title);
       setContent(post.content);
+
+      const author = data?.author;
+      const fullName = author.fullName;
+      const image = author.image;
+      const _id = author._id;
+
+      const createAt = data.createdAt;
+      setAuthor({
+        fullName: fullName,
+        image: image ?? PROFIE_IMG_URL,
+        createAt: createAt,
+        _id: _id,
+      });
     }
   }, [data]);
 
@@ -47,9 +74,13 @@ const DetailPost = () => {
   return (
     <ErrorBoundary>
       <Container>
-        <h1>Detail Post Page</h1>
-        <h1>제목: {title}</h1>
-        <textarea value={content} disabled rows={10} cols={100} />
+        <h1>{title}</h1>
+        <p>작성일: {author.createAt}</p>
+        <Author onClick={() => navigate(`/user/${author._id}`)}>
+          <p>작성자: {author.fullName}</p>
+          <ProfileImg src={author.image} />
+        </Author>
+        <Textarea value={content} disabled rows={10} cols={100} />
         <br />
         {token ? <button onClick={handleOnClickToUpdatePage}>내용 수정 페이지로 가기</button> : null}
         <Comment />
@@ -58,5 +89,18 @@ const DetailPost = () => {
     </ErrorBoundary>
   );
 };
+
+const Textarea = styled.textarea`
+  resize: none;
+`;
+
+const Author = styled.div`
+  background-color: #e6dada;
+`;
+
+const ProfileImg = styled.img`
+  border-radius: 50%;
+  width: 50px;
+`;
 
 export default DetailPost;
