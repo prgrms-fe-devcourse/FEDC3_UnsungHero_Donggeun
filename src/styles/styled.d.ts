674abc1b235b472aff0d5174ae43e10a36fb74a8
유지영 <yj0zero@gmail.com>
2023-01-17 02:29:58 +0900

    design: 💄 전역 스타일 추가 설정

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -6,12 +6,25 @@ declare module 'styled-components' {
       primary: string;
       white: string;
       black: string;
+      background: string;
+      lightGray: string;
+      gray: string;
     };
 
     fontSize: {
       title: number;
+      channelName: number;
+      likes: number;
       subTitle: number;
       content: number;
+      detailTitle: number;
+      detailContent: number;
+      detailUser: number;
+      detailLikse: number;
+      comment: number;
+      commentUser: number;
+      notification: number;
+      notificationContent: number;
     };
   }
 }
