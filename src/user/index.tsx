68db164141554bc9db2d705c1fdd7558518513a2
유지영 <yj0zero@gmail.com>
2023-01-18 00:57:13 +0900

    design: 💄 유저관련 페이지 스타일 적용

--- src/user/index.tsx
+++ src/user/index.tsx
@@ -15,7 +15,7 @@ interface IUserInfo {
   coverImage: string;
 }
 
-const COVER_IMG_URL = 'https://ifh.cc/g/xBfBwB.png';
+const COVER_IMG_URL = 'https://ifh.cc/g/XbvQvj.png';
 const LIKE_IMG_URL = 'https://ifh.cc/g/vmscWK.png';
 const API_URL = 'http://kdt.frontend.3rd.programmers.co.kr:5006';
 
@@ -51,32 +51,107 @@ const User = () => {
     navigate(`/userEdit/${currentPageId}`);
   };
   const totalLikes = userInfo?.posts?.reduce((acc, cur: Pick<IPost, 'likes'>) => acc + cur.likes.length, 0);
-
+  console.log(userInfo?.posts?.length);
   return (
-    <>
+    <Wrapper>
       <CoverImg src={userInfo.coverImage} alt='커버 이미지' />
-      <Avatar src={data?.image} width={60} height={60} />
-      <div>{userInfo.fullName}</div>
-      <div>
-        <img src={LIKE_IMG_URL} />
-        {totalLikes}
-      </div>
-      {currentPageId === myUserId ? (
-        <Button text={'내정보 수정'} onClick={handlemoveEditPage} color='white' width={100} height={30} />
-      ) : (
-        followButton(currentPageId as string)
-      )}
-
-      <Link to={`/following/${currentPageId}`}>팔로잉 {userFollow?.following?.length}</Link>
-      <Link to={`/followers/${currentPageId}`}>팔로워 {userFollow?.followers?.length}</Link>
-      {userInfo?.posts?.length > 0 && <UserPosts posts={userInfo.posts} />}
-    </>
+      <UserWrapper>
+        <Avatar src={data?.image} width={90} height={90} style={{ position: 'relative', top: '-1.875rem' }} />
+        <InfoWrapper>
+          <UserName>{userInfo.fullName}</UserName>
+          <Likes>
+            <img src={LIKE_IMG_URL} width='18px' height='18px' />
+            {totalLikes}
+          </Likes>
+        </InfoWrapper>
+        {currentPageId === myUserId ? (
+          <Button
+            text={'내정보 수정'}
+            onClick={handlemoveEditPage}
+            color='white'
+            width={100}
+            height={30}
+            style={{ marginLeft: 'auto' }}
+          />
+        ) : (
+          followButton(currentPageId as string)
+        )}
+      </UserWrapper>
+      <Follow>
+        <Link to={`/following/${currentPageId}`}>
+          팔로잉 <span>{userFollow?.following?.length}</span>
+        </Link>
+        <Link to={`/followers/${currentPageId}`}>
+          팔로워 <span>{userFollow?.followers?.length}</span>
+        </Link>
+      </Follow>
+      <List>
+        {userInfo?.posts?.length > 0 ? <UserPosts posts={userInfo.posts} /> : <p>작성한 글이 없습니다.</p>}
+      </List>
+    </Wrapper>
   );
 };
 
 export default User;
 
 const CoverImg = styled.img`
-  width: 200px;
-  height: 100px;
+  display: block;
+  width: 100%;
+  height: 11.25rem;
+`;
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.white};
+  max-width: 45.313rem;
+  border: 1px solid black;
+  height: 100%;
+  min-height: 640px;
+  border: 1px solid ${({ theme }) => theme.colors.boxLine};
+  box-shadow: 0px 4px 4px ${({ theme }) => theme.colors.shadow};
+  border-radius: 5px;
+  position: relative;
+  z-index: 5;
+`;
+
+const UserName = styled.div`
+  font-size: ${({ theme }) => theme.fontSize.large};
+  font-weight: bold;
+`;
+
+const UserWrapper = styled.div`
+  padding: 0.625rem 1.25rem;
+  height: 5rem;
+  display: flex;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  padding-left: 0.625rem;
+  flex-direction: column;
+`;
+
+const Follow = styled.div`
+  padding-left: 1.25rem;
+  & span {
+    font-weight: bold;
+    padding-right: 0.4375rem;
+  }
+`;
+
+const List = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  margin-top: 1.25rem;
+  border-top: 1px solid ${({ theme }) => theme.colors.contentLine};
+`;
+
+const Likes = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100%;
+  & img {
+    margin-right: 0.3125rem;
+  }
 `;
