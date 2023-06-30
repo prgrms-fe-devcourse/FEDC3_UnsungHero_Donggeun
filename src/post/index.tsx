fb97b74147deabc3ad444d7df5c81a49b04fdfc6
DaeHyun_Woo <po668312@naver.com>
2023-06-30 15:07:18 +0900

    chore: 불필요한 코드 삭제

--- src/post/index.tsx
+++ src/post/index.tsx
@@ -6,11 +6,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 function Post() {
   const location = useLocation();
   const currentPath = location.pathname;
-  const navigate = useNavigate();
   const videoUrl = 'https://youtu.be/gS8CALiBvEM';
-
-  console.log(currentPath);
-  console.log(navigate);
 
   useEffect(() => {
     if (currentPath === '/') {
