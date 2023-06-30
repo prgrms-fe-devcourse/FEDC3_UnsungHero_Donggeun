0e47a1f970d39c1a5ef97a2663ed371fb73c1a18
DaeHyun_Woo <po668312@naver.com>
2023-06-30 14:59:07 +0900

    fix: 유튜브 시연영상으로 redirect

--- src/post/index.tsx
+++ src/post/index.tsx
@@ -1,6 +1,23 @@
+import { useEffect } from 'react';
 import ErrorBoundary from '../api/ErrorBoundary';
 import Search from '../search/Index';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 function Post() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const navigate = useNavigate();
+  const videoUrl = 'https://youtu.be/gS8CALiBvEM';
+
+  console.log(currentPath);
+  console.log(navigate);
+
+  useEffect(() => {
+    if (currentPath === '/') {
+      window.location.href = videoUrl;
+    }
+  }, []);
+
   return (
     <>
       <ErrorBoundary>
